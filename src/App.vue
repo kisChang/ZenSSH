27e10eaa5b8b143037d2262dcfe772ab7dab0378
KisChang <734615869@qq.com>
2026-04-22 14:51:10 +0800

    抽取出IndexPc

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,28 @@
 <template>
   <div class="container">
   <el-config-provider :locale="locale">
-    <index-mobile v-if="isMobile" :is-loading="isLoading"/>
-    <el-container v-else-if="isPc">
-      <el-header v-loading="isLoading" height="40px" class="header">
-        <el-menu mode="horizontal"
-                 class="app-menu"
-                 menu-trigger="click"
-                 :default-active="activeMenu"
-                 :router="false"
-                 @select="handleMenuSelect"
-                 data-tauri-drag-region>
-<!--          <el-sub-menu index="1">
-            <template #title>文件</template>
-            <el-menu-item index="2-1">导入配置</el-menu-item>
-            <el-menu-item index="2-2">保存配置</el-menu-item>
-            <el-divider />
-            <el-menu-item index="2-3" @click="appExit">退出</el-menu-item>
-          </el-sub-menu>-->
-          <el-menu-item index="2" @click="toggleSessionManage">{{ $t('main.host') }}</el-menu-item>
-          <el-menu-item index="3" @click="openSetting">{{ $t('main.setting') }}</el-menu-item>
-          <el-menu-item index="4" @click="handleCheckUpdate(true)">{{update ? ("检测到更新：" + update.version) : "检查更新"}}</el-menu-item>
-          <el-menu-item index="5" @click="showAbout">{{ $t('main.about') }}</el-menu-item>
-        </el-menu>
-
-        <el-dialog
-            v-model="showUpdater"
-            title="检查到更新"
-            width="500">
-          <span>{{ update }}</span>
-          <template #footer>
-            <div class="dialog-footer">
-              <el-button @click="showUpdater = false">Cancel</el-button>
-              <el-button type="primary" @click="handleAppUpdate">
-                立即更新
-              </el-button>
-            </div>
-          </template>
-        </el-dialog>
-      </el-header>
-      <el-splitter>
-        <el-splitter-panel :min="200" :size="asideSize">
-          <connect-manage ref="connectManage"/>
-        </el-splitter-panel>
-        <el-splitter-panel :min="200">
-          <terminal-tabs ref="terminalTabs"/>
-        </el-splitter-panel>
-      </el-splitter>
-    </el-container>
+    <index-mobile v-if="isMobile"
+                  :is-loading="isLoading"/>
+    <index-pc v-else-if="isPc"
+              :is-loading="isLoading"/>
   </el-config-provider>
   </div>
 </template>
 
 <script>
-import {check} from '@tauri-apps/plugin-updater';
-import {openUrl} from '@tauri-apps/plugin-opener'
 import {listen} from '@tauri-apps/api/event'
-import {relaunch, exit} from '@tauri-apps/plugin-process'
 import ConnectManage from "./views/ConnectManage.vue";
 import TerminalTabs from "./views/TerminalTabs.vue";
-import {appConfigStore, appRunState, useMngStore, useTabsStore} from "@/store.js";
+import {appConfigStore, appRunState, useMngStore} from "@/store.js";
 import IndexMobile from "@/IndexMobile.vue";
+import IndexPc from "@/IndexPc.vue";
 import {isMobile} from "@/commons.js";
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import en from 'element-plus/es/locale/lang/en'
 
 export default {
   name: "Tauri",
-  components: {IndexMobile, TerminalTabs, ConnectManage},
+  components: {IndexMobile, IndexPc, TerminalTabs, ConnectManage},
   data() {
     return {
       isLoading: true, locale: zhCn,
@@ -75,8 +30,6 @@ export default {
       asideSize: 200,
       activeMenu: "",
       isMobile: false, isPc: false,
-
-      showUpdater: false, update: null,
     }
   },
   async mounted() {
@@ -116,7 +69,6 @@ export default {
       this.asideSize = 0;
     } else {
       this.isPc = true;
-      this.handleCheckUpdate();
     }
     await this.$nextTick();
 
@@ -148,75 +100,6 @@ export default {
         this.notify({message: "加载云端数据成功", type: "success"})
         this.$forceUpdate()
       }
-    },
-    handleCheckUpdate(byUser) {
-      check().then(update => {
-        if (update) {
-          this.update = update;
-          this.showUpdater = true
-        } else {
-          if (byUser) {
-            this.notify({type: 'success', message: '暂无更新，开发者正在噼里啪啦中...'});
-          }
-        }
-      }).catch(err => {
-        this.notify({type: 'primary', message: '暂无更新，因为开发者弄坏了更新服务器:' + err});
-      })
-    },
-    async handleAppUpdate() {
-      let downloaded = 0;
-      let contentLength = 0;
-      // alternatively we could also call update.download() and update.install() separately
-      await this.update.downloadAndInstall((event) => {
-        switch (event.event) {
-          case 'Started':
-            contentLength = event.data.contentLength;
-            console.log(`started downloading ${event.data.contentLength} bytes`);
-            break;
-          case 'Progress':
-            downloaded += event.data.chunkLength;
-            console.log(`downloaded ${downloaded} from ${contentLength}`);
-            break;
-          case 'Finished':
-            console.log('download finished');
-            break;
-        }
-      });
-      console.log('update installed');
-      await relaunch();
-    },
-    handleMenuSelect(index) {
-      this.activeMenu = index
-      this.$nextTick(() => {
-        this.activeMenu = null
-      })
-    },
-    toggleSessionManage() {
-      if (this.asideSize > 0) {
-        this.asideSize = 0;
-      } else {
-        this.asideSize = 200;
-      }
-    },
-    openSetting() {
-      for (let conn of useTabsStore().connList) {
-        if (conn.type === 'setting') {
-          this.$refs.terminalTabs.activeTab = 'setting';
-          return;
-        }
-      }
-      useTabsStore().connList.push({
-        id: 'setting',
-        type: 'setting',
-        title: '设置',
-        state: 1
-      })
-    },
-    appExit() {
-      exit(0).then()
-    },
-    showAbout(){
-      openUrl("https://blog.kischang.top").then()
     },
   }
 }
@@ -261,31 +144,7 @@ export default {
     transform: translateX(-50%) !important;
   }
 }
-</style>
 
-<style lang="scss" scoped>
-.container {
-  display: flex;
-  flex-direction: column;
-  height: calc(100vh - env(safe-area-inset-top) - env(safe-area-inset-bottom));
-  padding-top: env(safe-area-inset-top);
-  padding-bottom: env(safe-area-inset-bottom);
-  .header {
-    height: 30px;
-    padding: 0;
-    text-align: right;
-    background: #525252;
-  }
-  .aside-hidden {
-    width: 0 !important;
-  }
-  :deep(.el-splitter) {
-    height: calc(100vh - 30px);
-  }
-}
-</style>
-
-<style>
 :root {
   font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
   font-size: 16px;
@@ -315,26 +174,24 @@ html, body {
 }
 
 .container {
-  margin: 0;
-  padding: 0;
   display: flex;
   flex-direction: column;
+  height: calc(100vh - env(safe-area-inset-top) - env(safe-area-inset-bottom));
+  padding-top: env(safe-area-inset-top);
+  padding-bottom: env(safe-area-inset-bottom);
+  .header {
+    height: 30px;
+    padding: 0;
+    background: #525252;
+  }
+  .aside-hidden {
+    width: 0 !important;
+  }
+  :deep(.el-splitter) {
+    height: calc(100vh - 30px);
+  }
 }
 
-.el-menu--horizontal {
-  --el-menu-horizontal-height: 100%;
-  user-select: none;
-  .el-menu-item {
-    padding: 0 10px;
-    color: var(--el-menu-text-color) !important;
-    &:hover {
-      color: var(--el-menu-active-color) !important;
-    }
-  }
-  .el-divider--horizontal {
-    margin: 5px 0 !important;
-  }
-}
 .btn {
   width: 100%;
   padding: 12px;
---
+++ src/IndexPc.vue
@@ -0,0 +1,170 @@
+<template>
+  <el-container>
+    <el-header v-loading="isLoading" height="40px" class="header">
+      <el-menu mode="horizontal"
+               class="app-menu"
+               menu-trigger="click"
+               :default-active="activeMenu"
+               :router="false"
+               @select="handleMenuSelect"
+               data-tauri-drag-region>
+        <!--          <el-sub-menu index="1">
+                    <template #title>文件</template>
+                    <el-menu-item index="2-1">导入配置</el-menu-item>
+                    <el-menu-item index="2-2">保存配置</el-menu-item>
+                    <el-divider />
+                    <el-menu-item index="2-3" @click="appExit">退出</el-menu-item>
+                  </el-sub-menu>-->
+        <el-menu-item index="2" @click="toggleSessionManage">{{ $t('main.host') }}</el-menu-item>
+        <el-menu-item index="3" @click="openSetting">{{ $t('main.setting') }}</el-menu-item>
+        <el-menu-item index="4" @click="handleCheckUpdate(true)">{{update ? ("检测到更新：" + update.version) : "检查更新"}}</el-menu-item>
+        <el-menu-item index="5" @click="showAbout">{{ $t('main.about') }}</el-menu-item>
+      </el-menu>
+
+      <el-dialog
+          v-model="showUpdater"
+          title="检查到更新"
+          width="500">
+        <div>
+          <div>当前版本：{{update.currentVersion}}</div>
+          <div>最新版本：{{update.version}}</div>
+          <div>发布时间：{{update.date}}</div>
+          <div>更新内容：{{update.body || "暂无说明"}}</div>
+        </div>
+        <template #footer>
+          <div class="dialog-footer">
+            <el-button @click="showUpdater = false">Cancel</el-button>
+            <el-button type="primary" @click="handleAppUpdate">
+              立即更新
+            </el-button>
+          </div>
+        </template>
+      </el-dialog>
+    </el-header>
+    <el-splitter>
+      <el-splitter-panel :min="200" :size="asideSize">
+        <connect-manage ref="connectManage"/>
+      </el-splitter-panel>
+      <el-splitter-panel :min="200">
+        <terminal-tabs ref="terminalTabs"/>
+      </el-splitter-panel>
+    </el-splitter>
+  </el-container>
+</template>
+
+<script>
+import {check} from '@tauri-apps/plugin-updater';
+import {openUrl} from '@tauri-apps/plugin-opener'
+import {exit, relaunch} from '@tauri-apps/plugin-process'
+import ConnectManage from "./views/ConnectManage.vue";
+import TerminalTabs from "./views/TerminalTabs.vue";
+import {useTabsStore} from "@/store.js";
+
+export default {
+  name: "IndexPc",
+  components: {TerminalTabs, ConnectManage},
+  props: {
+    isLoading: false,
+  },
+  data() {
+    return {
+      asideSize: 200,
+      activeMenu: "",
+      showUpdater: false,
+      update: null,
+    }
+  },
+  mounted() {
+    this.handleCheckUpdate();
+  },
+  methods: {
+    handleCheckUpdate(byUser) {
+      check().then(update => {
+        if (update) {
+          this.update = update;
+          this.showUpdater = true
+        } else {
+          if (byUser) {
+            this.notify({type: 'success', message: '暂无更新，开发者正在噼里啪啦中...'});
+          }
+        }
+      }).catch(err => {
+        this.notify({type: 'primary', message: '暂无更新，因为开发者弄坏了更新服务器:' + err});
+      })
+    },
+    async handleAppUpdate() {
+      let downloaded = 0;
+      let contentLength = 0;
+      // alternatively we could also call update.download() and update.install() separately
+      await this.update.downloadAndInstall((event) => {
+        switch (event.event) {
+          case 'Started':
+            contentLength = event.data.contentLength;
+            console.log(`started downloading ${event.data.contentLength} bytes`);
+            break;
+          case 'Progress':
+            downloaded += event.data.chunkLength;
+            console.log(`downloaded ${downloaded} from ${contentLength}`);
+            break;
+          case 'Finished':
+            console.log('download finished');
+            break;
+        }
+      });
+      console.log('update installed');
+      await relaunch();
+    },
+    handleMenuSelect(index) {
+      this.activeMenu = index
+      this.$nextTick(() => {
+        this.activeMenu = null
+      })
+    },
+    toggleSessionManage() {
+      if (this.asideSize > 0) {
+        this.asideSize = 0;
+      } else {
+        this.asideSize = 200;
+      }
+    },
+    openSetting() {
+      for (let conn of useTabsStore().connList) {
+        if (conn.type === 'setting') {
+          this.$refs.terminalTabs.activeTab = 'setting';
+          return;
+        }
+      }
+      useTabsStore().connList.push({
+        id: 'setting',
+        type: 'setting',
+        title: '设置',
+        state: 1
+      })
+    },
+    appExit() {
+      exit(0).then()
+    },
+    showAbout(){
+      openUrl("https://blog.kischang.top").then()
+    },
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.el-menu--horizontal {
+  --el-menu-horizontal-height: 100%;
+  user-select: none;
+  .el-menu-item {
+    padding: 0 10px;
+    color: var(--el-menu-text-color) !important;
+    &:hover {
+      color: var(--el-menu-active-color) !important;
+    }
+  }
+  .el-divider--horizontal {
+    margin: 5px 0 !important;
+  }
+}
+</style>
+
